8c013ca13fcf527b4aa7e9bc41221a48f21fed68
曾廷睿 <55479277+juiajui@users.noreply.github.com>
2024-12-31 15:16:21 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,19 @@
 self.addEventListener('install', event => {
-    console.log('Service Worker 安裝中...');
+    console.log('[Service Worker] 安裝完成');
     event.waitUntil(
-        caches.open('pwa-cache-v1').then(cache => {
+        caches.open('pwa-demo-cache').then(cache => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/icon.png'
+                './',
+                './index.html',
+                './manifest.json',
+                './icon.png'
             ]);
         })
     );
 });
 
-self.addEventListener('activate', event => {
-    console.log('Service Worker 已啟用');
-});
-
 self.addEventListener('fetch', event => {
+    console.log('[Service Worker] 正在攔截請求: ', event.request.url);
     event.respondWith(
         caches.match(event.request).then(response => {
             return response || fetch(event.request);
